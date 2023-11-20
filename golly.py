371ed4669f0939b009c5d9e29cdec2d87d92de3c
hhkit <38537881+hhkit@users.noreply.github.com>
2023-11-20 19:36:32 +0800

    ✅ enabled profiling in all detectors

--- golly.py
+++ golly.py
@@ -15,11 +15,13 @@ golly_repair_path = "./build/barrier-repair/golly-repair"
 timings = {}
 profile = None
 
+
 class FuncDetection:
     def __init__(self, name, blockDim, gridDim):
         self.name = name
         self.blockDim = blockDim
         self.gridDim = gridDim
+
 
 def compile(
     filename: path.Path, workdir: path.Path, showWarnings: bool, clangArgs: [] = None
@@ -63,24 +65,41 @@ def canonicalize(outfile: io.TextIOWrapper, workdir: path.Path):
 
 def analyze(file: path.Path, patchFile: path.Path, config: path.Path, verbose: bool):
     options = {}
-    if config is not None: options["config"] = config 
-    if patchFile is not None: options["outfile"] = patchFile
-    if verbose: options["verbose"] = None
+    if config is not None:
+        options["config"] = config
+    if patchFile is not None:
+        options["outfile"] = patchFile
+    if verbose:
+        options["verbose"] = None
 
-    optionStr = f"<{';'.join(f'{k}={v}' if v is not None else f'{k}' for k,v in options.items())}>"  if len(options) > 0 else ""
+    optionStr = (
+        f"<{';'.join(f'{k}={v}' if v is not None else f'{k}' for k,v in options.items())}>"
+        if len(options) > 0
+        else ""
+    )
 
-    cmd = [ "opt",
-            f"--load-pass-plugin={golly_path}",
-            "--passes=golly" + optionStr,
-            "--disable-output",
-            file.resolve(),
-          ]
+    cmd = [
+        "opt",
+        f"--load-pass-plugin={golly_path}",
+        "--passes=golly" + optionStr,
+        "--disable-output",
+        file.resolve(),
+    ]
     # + ([f"--golly-out={patchFile}" if patchFile is not None else []])
     # print("command: " + " ".join(map(str, cmd)))
     sp.run(cmd)
 
 
-def analysisPass(filename, workdir, patchFile, showWarnings, clangArgs, verbose,  config: path.Path = None, **kwargs):
+def analysisPass(
+    filename,
+    workdir,
+    patchFile,
+    showWarnings,
+    clangArgs,
+    verbose,
+    config: path.Path = None,
+    **kwargs,
+):
     file = filename
     if file.suffix == ".cu":
         # compile and canonicalize
@@ -95,40 +114,62 @@ def analysisPass(filename, workdir, patchFile, showWarnings, clangArgs, verbose,
     assert file.exists()
 
     durs = []
-    for i in range(kwargs['iters']):
+    for i in range(kwargs["iters"]):
         start = time.perf_counter()
         analyze(file, patchFile=patchFile, config=config, verbose=verbose)
         end = time.perf_counter()
         dur = end - start
         durs.append(dur)
+    print(patchFile)
+    err = "-"
+    try:
+        with open(patchFile) as errfile:
+            for error in yaml.load(errfile, Loader=yaml.Loader):
+                err = f"{error['type']}.{error['level']}"
+    except IOError:
+        "no error"
 
     timings[str(filename)] = {
-        'times': durs,
-        'min': min(durs),
-        'max': max(durs),
-        'avg': mean(durs)
+        "times": durs,
+        "min": min(durs),
+        "max": max(durs),
+        "avg": mean(durs),
+        "error": err,
     }
+
 
 def raceDetected(patchFile: path.Path) -> bool:
     return patchFile.exists() and patchFile.stat().st_size > 2
 
-def repair(file: path.Path, workdir:path.Path, clangArgs, patchFile: path.Path, blockDim: str, gridDim: str):
+
+def repair(
+    file: path.Path,
+    workdir: path.Path,
+    clangArgs,
+    patchFile: path.Path,
+    blockDim: str,
+    gridDim: str,
+):
     scratch = path.Path(f"{workdir}/{uuid.uuid4()}")
     tmp_dir = path.Path(f"{workdir}/patches")
 
     tmp_dir.mkdir(parents=True, exist_ok=True)
 
-    sp.run([
-        golly_repair_path,
-        file,
-        f"--locsFile={patchFile}",
-        f"--golly-tmp={tmp_dir}"
-    ])
+    sp.run(
+        [golly_repair_path, file, f"--locsFile={patchFile}", f"--golly-tmp={tmp_dir}"]
+    )
 
     for f in tmp_dir.glob("*"):
         print(f"attempt repair with {f}")
         workdir2 = path.Path(f"{workdir}/{hash(f)}")
-        analysisPass(f, workdir=workdir2, patchFile=scratch, showWarnings=False, clangArgs=clangArgs, verbose=False)
+        analysisPass(
+            f,
+            workdir=workdir2,
+            patchFile=scratch,
+            showWarnings=False,
+            clangArgs=clangArgs,
+            verbose=False,
+        )
         if raceDetected(scratch):
             print(f"repair failed with {f}")
             scratch.unlink()
@@ -136,7 +177,8 @@ def repair(file: path.Path, workdir:path.Path, clangArgs, patchFile: path.Path, 
             print(f"repair success with {f}")
             return
 
-    print('could not repair race')
+    print("could not repair race")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -163,12 +205,8 @@ if __name__ == "__main__":
         type=int,
         default=1,
     )
-    parser.add_argument(
-        "--profile",
-        help="Profiles the analysis",
-        type=path.Path
-    )
-    
+    parser.add_argument("--profile", help="Profiles the analysis", type=path.Path)
+
     parser.add_argument(
         "--repair",
         "-r",
@@ -182,22 +220,26 @@ if __name__ == "__main__":
 
     profile = args.profile
 
-
     if args.filename.suffix == ".yaml":
-        with open(args.filename, 'r') as file:
+        with open(args.filename, "r") as file:
             dir = os.path.splitext(args.filename)[0]
             for item in yaml.safe_load(file):
-                f = item['file']
-                kernels = item.get('kernels')
-                assert(type(kernels) == type([])) # kernels should be an array 
+                f = item["file"]
+                kernels = item.get("kernels")
+                assert type(kernels) == type([])  # kernels should be an array
 
                 workdir = path.Path(f"{tempfile.gettempdir()}/golly/{uuid.uuid4()}")
                 workdir.mkdir(parents=True, exist_ok=True)
-                configFile=f"{workdir}/cfg"
-                with open(configFile, 'w') as cfg:
+                configFile = f"{workdir}/cfg"
+                with open(configFile, "w") as cfg:
                     yaml.safe_dump(kernels, cfg)
-                patchFile=f"{workdir}/pairs.out"
-                analysisPass(workdir=workdir, patchFile=patchFile, config=configFile, **dict(vars(args), filename=path.Path(f"{dir}/{f}")))
+                patchFile = f"{workdir}/pairs.out"
+                analysisPass(
+                    workdir=workdir,
+                    patchFile=patchFile,
+                    config=configFile,
+                    **dict(vars(args), filename=path.Path(f"{dir}/{f}")),
+                )
                 print("\n")
             if profile is not None:
                 with open(profile, "w") as outFile:
@@ -206,8 +248,15 @@ if __name__ == "__main__":
     else:
         workdir = path.Path(f"{tempfile.gettempdir()}/golly/{uuid.uuid4()}")
         workdir.mkdir(parents=True, exist_ok=True)
-        patchFile=f"{workdir}/pairs.out"
+        patchFile = f"{workdir}/pairs.out"
         analysisPass(workdir=workdir, patchFile=patchFile, **vars(args))
 
         if args.repair:
-            repair(args.filename, workdir, clangArgs=args.clangArgs, patchFile=patchFile, blockDim=args.blockDim, gridDim=args.gridDim )
+            repair(
+                args.filename,
+                workdir,
+                clangArgs=args.clangArgs,
+                patchFile=patchFile,
+                blockDim=args.blockDim,
+                gridDim=args.gridDim,
+            )
--- tests/gklee.py
+++ tests/gklee.py
@@ -4,21 +4,37 @@ import subprocess as sp
 import os
 import yaml
 import argparse
+import re
 import docker
 import time
 from statistics import mean
 
 parser = argparse.ArgumentParser(
     prog="GKLEE driver",
-    )
+)
 parser.add_argument("filename", type=path.Path)
 parser.add_argument("--profile", action="store_true")
 parser.add_argument("--profile-out", dest="profileOut", type=path.Path)
 parser.add_argument("--iters", type=int, default=1)
 args = parser.parse_args()
 
+
 def strip(s):
-  return str(s).replace(' ', '')
+    return str(s).replace(" ", "")
+
+
+race_re = re.compile("incur the \(Actual\) (read|write)-write race")
+bd_re = re.compile("Found a deadlock")
+
+
+def extract_error(ec, log):
+    if bd_re.search(log) != None:
+        return "bd"
+    if race_re.search(log) != None:
+        return "race"
+
+    return "-"
+
 
 profile = bool(args.profile)
 iters = args.iters
@@ -26,57 +42,71 @@ iters = args.iters
 timings = {}
 
 if args.filename.suffix == ".yaml":
-  with open(args.filename, 'r') as file:
-    dir = os.path.splitext(args.filename)[0]
+    with open(args.filename, "r") as file:
+        dir = os.path.splitext(args.filename)[0]
 
-    for file in yaml.safe_load(file):
-      kernels = file['kernels']
-      # take the first kernel
-      cfg = kernels[0]
+        for file in yaml.safe_load(file):
+            kernels = file["kernels"]
+            # take the first kernel
+            cfg = kernels[0]
 
-      filename=path.Path(str(dir) + "/" + str(file['file']))
+            filename = path.Path(str(dir) + "/" + str(file["file"]))
 
-      b = strip(str(cfg['block']))
-      g = strip(str(cfg['grid']))
+            b = strip(str(cfg["block"]))
+            g = strip(str(cfg["grid"]))
 
-      client = docker.from_env()
-      cont = client.containers.run(image="gklee", volumes=[
-        f"{os.getcwd()}/tests:/mnt/tests"
-      ], command="bash", detach=True, tty=True)
+            client = docker.from_env()
+            cont = client.containers.run(
+                image="gklee",
+                volumes=[f"{os.getcwd()}/tests:/mnt/tests"],
+                command="bash",
+                detach=True,
+                tty=True,
+            )
 
-      def exec():
-        base_name =  os.path.basename(os.path.splitext(filename)[0])
-        (ec,out) = cont.exec_run(f"bash -c 'gklee-nvcc /mnt/{filename} -o {base_name}.bc -I/mnt/{os.path.dirname(filename)} -I/usr/include '")
-        (ec,out) = cont.exec_run(f"bash -c 'timeout 1m gklee --verbose 0 --symbolic-config --race-prune {base_name}.bc'")
-        
-        return out.decode('UTF-8')
+            base_name = os.path.basename(os.path.splitext(filename)[0])
+            (ec, out) = cont.exec_run(
+                f"bash -c 'gklee-nvcc /mnt/{filename} -o {base_name}.bc -I/mnt/{os.path.dirname(filename)} -I/usr/include '"
+            )
 
-      if (profile):
-        for i in range(iters):
-          start = time.perf_counter()
-          out = exec()
-          end = time.perf_counter()
-          dur = end - start
-          if str(filename) not in timings:
-            timings[str(filename)] = { 'times': [dur], 'log': out}
-          else: 
-            timings[str(filename)]['times'].append(dur)
-      else: 
-        exec()
-      print(filename)
+            def exec():
+                (ec, out) = cont.exec_run(
+                    f"bash -c 'timeout 1m gklee --verbose 0 --symbolic-config --race-prune {base_name}.bc'"
+                )
 
-      cont.stop()
-      cont.remove()
+                return (ec, out.decode("UTF-8"))
 
-  for k,v in timings.items():
-    times = v['times']
-    timings[k]['min'] = min(times)
-    timings[k]['avg'] = mean(times)
-    timings[k]['max'] = max(times)
-    
-  if args.profileOut is not None:
-    with open(args.profileOut, "w") as outFile:
-      outFile.write(yaml.dump(timings))
-  else:
-    print(str(timings))
-  
+            if profile:
+                for i in range(iters):
+                    start = time.perf_counter()
+                    ec, out = exec()
+                    end = time.perf_counter()
+                    dur = end - start
+
+                    if str(filename) not in timings:
+                        err = extract_error(ec, out)
+                        timings[str(filename)] = {
+                            "times": [dur],
+                            "error": err,
+                            "log": out,
+                        }
+                        print(f"{filename}: {err}")
+                    else:
+                        timings[str(filename)]["times"].append(dur)
+            else:
+                exec()
+
+        cont.stop()
+        cont.remove()
+
+    for k, v in timings.items():
+        times = v["times"]
+        timings[k]["min"] = min(times)
+        timings[k]["avg"] = mean(times)
+        timings[k]["max"] = max(times)
+
+    if args.profileOut is not None:
+        with open(args.profileOut, "w") as outFile:
+            outFile.write(yaml.dump(timings))
+    else:
+        print(str(timings))
